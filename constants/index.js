edbf5201f085539a55bbfe53a61b7c066427c0de
RAJA LAKSHMI R <127002476+RajalakshmiR24@users.noreply.github.com>
2024-03-25 21:59:54 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,53 @@
+export const rapidApiKey = '2869b29785mshd67011e71bc4276p1874b6jsn679ae0457fbc';
+// below is the api used from rapid api hub
+// https://rapidapi.com/ytdlfree/api/youtube-v3-alternative/
+
+export const categories = [
+  'All',
+  'New to you',
+  'Gaming',
+  'React Routers',
+  'Music', 
+  'John Snow',
+  'Live', 
+  'Gadgets',
+  'Programming',
+  'Podcasts',
+  'Computers',
+  'Others'
+]
+
+export const shortVideos = [
+  {
+    id: 2,
+    title: 'Reasons you should learn React Native',
+    viewCount: '355k',
+    image: require('../assets/images/short2.png')
+  },
+  {
+    id: 2,
+    title: "Mobile app development",
+    viewCount: '132k',
+    image: require('../assets/images/short5.png')
+  },
+  {
+    id: 3,
+    title: "Tesla's Wireless Charger: Explained!",
+    viewCount: '2M',
+    image: require('../assets/images/short3.png')
+  },
+  {
+    id: 4,
+    title: "Rare Moments in Football 🥶🤩",
+    viewCount: '167k',
+    image: require('../assets/images/short4.png')
+  },
+  {
+    id: 5,
+    title: 'Fastest Way to Learn React in 2023 🔥',
+    viewCount: '1.2M',
+    image: require('../assets/images/short1.png')
+  },
+  
+
+]
